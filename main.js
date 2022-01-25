d670618cb1648a9ab519022c7d59d392b945c3e6
182exe <81325987+182exe@users.noreply.github.com>
2022-01-25 13:18:53 -0700

    Added settings.
    Added options to put settings before generating a level.

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const vowels = 'aeiou';
 const consonents = 'bcdfghjklmnprstvw';
 const rares = 'qxzy';
 
+var settingLength = 15;
+var settingTime = 3;
+
 function generateString(length) {
   let result = '';
   const vowelsLength = vowels.length;
@@ -188,76 +191,90 @@ function generateString(length) {
   return result;
 }
 
-const raw = (generateString(15));
-const raw2 = raw.trim();
-const raw3 = raw2 + '.'
-const str = raw3.charAt(0).toUpperCase() + raw3.slice(1);
-const text = document.querySelector(".text");
-const input = document.querySelector(".input");
-const progressBar = document.querySelector(".progress-bar");
-
-var time = str.length / 3;
-var totalTime = Math.round(time);
-var css = time+"s animate linear";
-var javatime = time*1000;
-
-let timer = null;
-
-const charEls = [];
-
-function populateText(str) {
-  str.split("").map(letter => {
-    const span = document.createElement("span");
-    span.innerText = letter;
-    text.appendChild(span);
-    charEls.push(span);
-  });
-}
-populateText(str);
-
-function resetCharEls() {
-  charEls.map(charEl => {
-    charEl.classList.remove("correct");
-    charEl.classList.remove("wrong");
-  });
-};
-
-input.addEventListener("keyup", () => {
-  if (!timer) {
-    progressBar.classList.add("active");
-    document.getElementById('progress-bar').style.animation = css;
-    timer = setTimeout(() => {
-      if (confirm("You ran out of time! You can close this notification to continue typing or press OK to retry.") === true) {
+function start(lengthsetting,timesetting) {
+  const raw = (generateString(lengthsetting));
+  const raw2 = raw.trim();
+  const raw3 = raw2 + '.'
+  const str = raw3.charAt(0).toUpperCase() + raw3.slice(1);
+  const text = document.querySelector(".text");
+  const input = document.querySelector(".input");
+  const progressBar = document.querySelector(".progress-bar");
+  
+  var time = str.length / timesetting;
+  var totalTime = Math.round(time);
+  var css = time+"s animate linear";
+  var javatime = time*1000;
+  
+  let timer = null;
+  
+  const charEls = [];
+  
+  function populateText(str) {
+    str.split("").map(letter => {
+      const span = document.createElement("span");
+      span.innerText = letter;
+      text.appendChild(span);
+      charEls.push(span);
+    });
+  }
+  populateText(str);
+  
+  function resetCharEls() {
+    charEls.map(charEl => {
+      charEl.classList.remove("correct");
+      charEl.classList.remove("wrong");
+    });
+  };
+  
+  input.addEventListener("input", () => {
+    if (!timer) {
+      progressBar.classList.add("active");
+      document.getElementById('progress-bar').style.animation = css;
+      timer = setTimeout(() => {
+        if (confirm("You ran out of time! You can close this notification to continue typing or press OK to retry.") === true) {
+          location.reload()
+        } else {
+          //nothing
+        }
+      }, javatime);
+    }
+    const val = input.value;
+    resetCharEls();
+    let errorCount = 0;
+    let correctCount = 0;
+    val.split("").map((letter, i) => {
+      if (letter === str[i]){
+        charEls[i].classList.add("correct");
+        correctCount = correctCount + 1
+      } else {
+        charEls[i].classList.add("wrong");
+        errorCount = errorCount + 1
+      };
+    });
+    if (val.length >= str.length || val.length === str.length) {
+      if (errorCount === 1) {
+        var resultText = "Well Done! You completed the test within the time limit of " + totalTime + " seconds with " + errorCount + " error!\n\nPress OK to restart and reload the page."
+      } else {
+        var resultText = "Well Done! You completed the test within the time limit of " + totalTime + " seconds with " + errorCount + " errors!\n\nPress OK to restart and reload the page."
+      }
+      if (confirm(resultText) === true) {
         location.reload()
       } else {
         //nothing
       }
-    }, javatime);
-  }
-  const val = input.value;
-  resetCharEls();
-  let errorCount = 0;
-  let correctCount = 0;
-  val.split("").map((letter, i) => {
-    if (letter === str[i]){
-      charEls[i].classList.add("correct");
-      correctCount = correctCount + 1
-    } else {
-      charEls[i].classList.add("wrong");
-      errorCount = errorCount + 1
-    };
+      clearTimeout(timer);
+    }
   });
-  if (val.length >= str.length || val.length === str.length) {
-    if (errorCount === 1) {
-      var resultText = "Well Done! You completed the test within the time limit of " + totalTime + " seconds with " + errorCount + " error!\n\nPress OK to restart and reload the page."
-    } else {
-      var resultText = "Well Done! You completed the test within the time limit of " + totalTime + " seconds with " + errorCount + " errors!\n\nPress OK to restart and reload the page."
-    }
-    if (confirm(resultText) === true) {
-      location.reload()
-    } else {
-      //nothing
-    }
-    clearTimeout(timer);
+}
+
+function updateSettings() {
+  if (document.getElementById("timeInput").value.length == 0 || document.getElementById("lengthInput").value.length == 0) {
+    window.alert('Please input values in the setting boxes before generating a level.')
+  } else {
+    var settingLength = document.getElementById("lengthInput").value;
+    var settingTime = document.getElementById("timeInput").value;
+    start(settingLength,settingTime);
   }
-});
+}
+
+//ok no more functions lol
