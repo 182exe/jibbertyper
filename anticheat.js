ea7442e872300c325c1f29851041ae12f8d1d772
182exe <81325987+182exe@users.noreply.github.com>
2022-02-01 14:36:31 -0700

    Update anticheat to look like watchdog (lol!)

--- anticheat.js
+++ anticheat.js
@@ -1,6 +1,6 @@
 function antiCheat() {
   document.getElementById("inputarea").innerHTML = "";
-  window.alert("[JibbertyperCheatDetection] You have been detected copying, cutting, or pasting content on this webpage. Your current progress has been wiped and you will now restart. Please refrain from cheating in the future.");
+  window.alert("[Watchdog Cheat Detection] You have been detected copying, cutting, or pasting content on this webpage. Your current progress has been wiped and you will now restart. Please refrain from cheating in the future.");
   location.reload();
 }
 
