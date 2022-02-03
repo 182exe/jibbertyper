317567da223851428c87db1be10eaf345a3ccbba
182exe <81325987+182exe@users.noreply.github.com>
2022-02-03 11:27:20 -0700

    Update anticheat.js

--- anticheat.js
+++ anticheat.js
@@ -1,7 +1,6 @@
 function antiCheat() {
   document.getElementById("inputarea").innerHTML = "";
   window.alert("[Watchdog Cheat Detection] You have been detected copying, cutting, or pasting content on this webpage. Your current progress has been wiped and you will now restart. Please refrain from cheating in the future.");
-  location.reload();
 }
 
 window.addEventListener("copy", antiCheat);
