688dd055858e37a0d50669248fa9f7dc87725f13
182exe <81325987+182exe@users.noreply.github.com>
2023-07-16 21:59:56 -0700

    big fat re code

--- anticheat.js
+++ anticheat.js
@@ -1,7 +1,7 @@
 function antiCheat() {
-  document.getElementById("inputarea").innerHTML = "";
-  window.alert("[Watchdog Cheat Detection] You have been detected pasting content on this webpage. Your attempt has been canceled and this page will be reloaded. Please refrain from cheating in the future.");
-  location.reload()
+    document.getElementById("inputarea").innerHTML = "";
+    window.alert("Do not paste content into this webpage. You may use it to generate fake words, but you may not use your clipboard to cheat the gamez :)");
+    location.reload()
 }
 
 window.addEventListener("paste", antiCheat);
